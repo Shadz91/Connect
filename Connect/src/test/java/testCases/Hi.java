0e71da69faa4ccc3db046de30218c203d1ace737
M1036018 <M1036018@A4ML24373.mindtree.com>
2019-04-08 22:44:02 +0530

    co

--- Connect/src/test/java/testCases/Hi.java
+++ Connect/src/test/java/testCases/Hi.java
@@ -2,7 +2,6 @@ package testCases;
 
 public class Hi {
 	public static void main(String args[]) {
-		System.out.println("oioi");
-		System.out.println("conflict time");	
+		System.out.println("oioi");	
 	}
 }
