cb9782ca74e210d0520e38be5a1ddcd7033f9a45
Shadz91 <37531040+Shadz91@users.noreply.github.com>
2019-04-08 22:45:58 +0530

    Update Hi.java

--- Connect/src/test/java/testCases/Hi.java
+++ Connect/src/test/java/testCases/Hi.java
@@ -2,6 +2,7 @@ package testCases;
 
 public class Hi {
 	public static void main(String args[]) {
-		System.out.println("oioi");	
+		System.out.println("oioi");
+		System.out.println("oioioioi");
 	}
 }
